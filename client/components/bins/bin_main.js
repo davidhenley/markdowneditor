24dbdd6e446775842d9bb1976b4188d4961f635d
davidhenley <henleydr@gmail.com>
2017-02-10 13:37:20 -0600

    Subscribed to Bins from BinMain

--- client/components/bins/bin_main.js
+++ client/components/bins/bin_main.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Bins } from '../../../imports/collections/bins';
 
 class BinMain extends Component {
   render() {
+    console.log(this.props.bin);
     return (
       <div className="bin-main">
         Bin Main {this.props.params.binId}
@@ -10,4 +14,8 @@ class BinMain extends Component {
   }
 }
 
-export default BinMain;
+export default createContainer((props) => {
+  const { binId } = props.params;
+  Meteor.subscribe('bins');
+  return { bin: Bins.findOne(binId) };
+}, BinMain);
