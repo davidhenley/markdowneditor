748872f23062de42f7e6f6449747bc71c8717009
davidhenley <henleydr@gmail.com>
2017-02-10 13:51:19 -0600

    Added CodeMirror

--- client/components/bins/bins_editor.js
+++ client/components/bins/bins_editor.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
 
 class BinsEditor extends Component {
   render() {
     return (
-      <div className="bins-editor">
-        Bins Editor
+      <div className="bins-editor col-xs-8">
+        <h5>Input</h5>
+        <CodeMirror options={{ mode: 'markdown', lineNumbers: true }} />
       </div>
     );
   }
