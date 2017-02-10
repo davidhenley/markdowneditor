f6275bf3b408a691e71e117979a78a31984b5d74
davidhenley <henleydr@gmail.com>
2017-02-10 10:17:12 -0600

    Changed BinsList to IndexRoute of root

--- client/components/app.js
+++ client/components/app.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Header from './header';
-import BinsList from './bins/bins_list';
 
 export default (props) => {
   return (
     <div className="app">
       <Header />
-      <BinsList />
       {props.children}
     </div>
   );
--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import BinsList from './components/bins/bins_list';
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={BinsList} />
+  </Route>
 );
